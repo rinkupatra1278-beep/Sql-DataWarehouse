df1ce84f7e82289fd6d5165d51d22e82b4287673
rinkupatra1278-beep <rinkupatra1278@gmail.com>
2026-02-13 16:50:16 +0530

    Update fix.sql

--- Script/silver/fix.sql
+++ Script/silver/fix.sql
@@ -100,3 +100,27 @@ INSERT INTO silver.crm_sales_details (
 				ELSE sls_price  -- Derive price if original value is invalid
 			END AS sls_price
 		FROM bronze.crm_sales_details;
+
+
+
+-- insert clean data in silver.erp_cust_az12
+
+insert into silver.erp_cust_az12(
+cid,
+bdate,
+gen)
+select 
+case 
+    when cid like 'NAS%' then substring(cid,4,len(cid))
+	else cid
+	end as cid_,
+case 
+   when bdate > getdate() then null
+   else bdate
+   end as bdate,
+   case
+   when upper(trim(gen)) in('M' , 'Male') then 'Male'
+   when upper(trim(gen)) in ('F', 'Female') then 'Female'
+   else 'n/a'
+   end as gen
+   from bronze.erp_cust_az12
